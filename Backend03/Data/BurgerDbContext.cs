e031fed0d225aadc71f480944d27d529b3f2079c
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2024-10-02 16:01:38 +0200

    dbcontext added

---
+++ Backend03/Data/BurgerDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Backend03.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend03.Data
+{
+    public class BurgerDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set;}
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=BurgerDb;Trusted_Connection=True;TrustServerCertificate=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public BurgerDbContext()
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
