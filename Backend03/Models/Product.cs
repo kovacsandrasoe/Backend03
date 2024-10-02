0e350863af5c7f231b2447877be66591ed85acf2
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2024-10-02 15:39:18 +0200

    models added

---
+++ Backend03/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend03.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public string Size { get; set; }
+
+        public Restaurant Restaurant { get; set; }
+
+    }
+}
---
+++ Backend03/Models/Restaurant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend03.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Product> Products { get; set; } 
+    }
+}
